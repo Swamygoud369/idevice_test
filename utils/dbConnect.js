845bcdd56ac3037a95c53da45b2ce27a98aa2a05
Swamygoud369 <goud.swamy369@gmail.com>
2023-08-08 19:47:24 +0530

    sa

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,23 +2,13 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
 
-const db_options = {dbName: "Idevice",};
+// const db_options = {dbName: "Idevice",};
 
 export const dbConnect = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017", db_options);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(`connected this`);
     }catch (err){
         console.log(err);
     }
 }
-
-
-// export const dbConnect = async () => {
-//     if(mongoose.connections[0]){
-//         return;
-//     }
-//     await mongoose.connect(process.env.MONGODB_URI, db_options)
-//     .then(() => console.log("db Connected"))
-//     .catch((err) => console.log(err))
-// };
